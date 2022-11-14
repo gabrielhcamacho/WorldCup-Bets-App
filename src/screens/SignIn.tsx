9d46d1cd8e67cb6ccb6c5c7d15700ab31950b226
Gabriel Camacho <gabriel.hcamacho@outlook.com>
2022-11-14 16:57:10 -0300

    home screen and button component created

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,26 @@
-import { Center, Text } from 'native-base'
-import React from 'react'
+
+import { Center, Text, Icon} from 'native-base'
+import { Fontisto } from '@expo/vector-icons'
+
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button'
+
 
 export function SignIn() {
     return (
-        <Center flex={1} bgColor="gray.900">
-            <Text color="white" fontSize={24}>Sign In</Text>
+        <Center flex={1} bgColor="gray.900" p={7}>
+           <Logo width={212} height={40}/>
+
+           <Button 
+            title="SIGN IN WITH GOOGLE" 
+            type='SECONDARY'
+            leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
+            mt={12}
+           />
+
+           <Text color="white" textAlign="center" mt={4}>
+             We don't use any information besides your {'\n'} email to create your account
+           </Text>
         </Center>
     )
 }
